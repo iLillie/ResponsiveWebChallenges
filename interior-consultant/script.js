37305b5308c3aafbc51be9c9bed7e36163185e7b
Lillie Rugtveit <75392499+iLillie@users.noreply.github.com>
2022-06-16 22:29:53 +0200

    Update A11y and cleaning up code

--- interior-consultant/script.js
+++ interior-consultant/script.js
@@ -1,7 +1,13 @@
 let mobileNavBg = document.getElementById('mobile-nav-bg');
-let mobileNavList = document.getElementById('nav-list');
+let mobileNavList = document.getElementById('main-menu');
 
-let openMenu = () => {
+let openMenu = (element) => {
+    ariaExpand(element);
     mobileNavBg.classList.toggle('menu-closed-bg');
     mobileNavList.classList.toggle('menu-open');
+}
+
+let ariaExpand = (element) => {
+    var ariaExpandedValue = element.getAttribute("aria-expanded") === 'true' ? true : false;
+    element.setAttribute('aria-expanded', !ariaExpandedValue)
 }
